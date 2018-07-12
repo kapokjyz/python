6163cac5935666cfaadeb322d209c9cf24592867
kapokjyz <kapok_jyz@163.com>
2018-07-12 08:57:54 +0800

    更新生成器学习代码

--- 15_generator.py
+++ 15_generator.py
@@ -1,5 +1,6 @@
 # 生成器：它是在 for 循环的过程中不断计算出下一个元素，并在适当的条件结束for循环
-# 创建一个generator， 有很多方法，第一种：把列表生成式的 [] 改成 () ，就创建了一个generator：
+# 创建一个generator， 有很多方法
+# 第一种：把列表生成式的 [] 改成 () ，就创建了一个generator：
 L = [x * x for x in range(10)]
 print(L)
 g = (x * x for x in range(10))
@@ -14,3 +15,56 @@ print(next(g))
 for n in g:
     print(n)
 
+# 案例：斐波拉契数列，用列表生成式写不出来，但是用函数把他打印出来却很容易
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        print(b)
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+fib(5)
+# 上面的赋值语句：a, b = b, a + b ;相当于：
+# t = (b, a + b) # t是一个tuple
+# a = t[0]
+# b = t[1]
+
+# fib()函数实际上是定义了斐波拉契数列的推算规则，可以从第一个元素开始，推算出后续任意的元素，这种逻辑其实非常类似 generator
+# 把fib()函数变成 generator ，只需要将print(b) 改成 yield b 就可以了：
+def fib1(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+# 包含了 yield 关键字的函数，是一个generator 
+f = fib1(5)
+print(f)
+print(next(f))
+for n in f:
+    print(n)
+# 记住，函数是顺序执行，遇到return或者最后一行就返回，而变成 generator 的函数，在每次调用next()的时候，遇到yield 语句返回，再次执行的时候从上次返回 yield 语句出执行
+
+# 调用 generator 时，发现拿不到 generator 的return语句返回值；如果想要拿到返回值，必须捕获 StopIteration 错误（再取完所有的值后再继续取值就报这个错），返回值包含再 StopIteration 的 value 中。
+
+g = fib1(6)
+while True:
+    try:
+        x = next(g)
+        print('g:', x)
+    except StopIteration as e:
+        print('Generator return value: ', e.value)
+        break
+
+# 练习：输出杨辉三角
+def triangles(lines):
+    n, m, a, b = 0, 0, 0, 1
+    L = [1]
+    while n < lines:
+        yield L
+        L =[1] + [L[i] + L[i+1] for i in range(len(L)-1)] + [1] 
+        n = n + 1
+g = triangles(5)
+for n in g:
+    print(n)        
