13801284ee54bee4f5c199b9d77bd4eaf5ff10cd
kapokjyz <kapok_jyz@163.com>
2018-07-15 12:10:52 +0800

    更新map和reduce高阶函数

--- 18_map_reduce.py
+++ 18_map_reduce.py
@@ -1,6 +1,5 @@
 # python内建了 map() 和 reduce() 函数
 # map() 函数接收两个参数，一个是函数，一个是 Iterable ,map 将传入的函数依次作用到序列的每个元素，并把结果作为新的 Iterator 返回。
-# map()
 def f(x):
     return x * x
 
@@ -12,7 +11,6 @@ print(L)
 L = list(map(str, [1,2,3,4,5,6]))
 print(L)
 
-# reduce()
 # reduce 把一个函数作用在一个序列 [x1, x2, x3, ...] 上，这个函数必须接收两个参数，例如：reduce 把结果和序列的下一个与阿奴做累计计算，其效果就是：
 # reduce(f, [x1, x2, x3, x4]) = f(f(f(x1, x2), x3), x4)
 from functools import reduce
@@ -20,3 +18,12 @@ def add(x, y):
     return x + y
 s = reduce(add, [1,3,5,7,9])
 print(s)
+
+# 使用 map() ，把str转换为int的函数：
+def fn(x, y):
+    return x * 10 + y
+def char2num(s):
+    digits = {'0':0, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9}
+    return digits[s]
+r = reduce(fn, map(char2num, '13579'))
+print(r)
