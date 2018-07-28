4f4947a2ee9d6c6f986e3785c2b189f11a7535e7
kapokjyz <kapok_jyz@163.com>
2018-07-28 18:11:57 +0800

    上传返回函数学习代码

--- 21_return _func.py
+++ 21_return _func.py
@@ -18,3 +18,36 @@ f = lazy_sum(*L)
 L.append(11)
 print(L)
 print(f())
+
+#  内部函数可以引用外部函数的参数和局部变量，这种称为闭包，这种程序结构具有极大的威力
+
+# 有一个需要注意的是，返回的函数没有立即执行，而是知道调用了f()才执行。
+# 例如：
+def count():
+    fs = []
+    for i in range(1, 4):
+        def f():
+            return i * i
+        fs.append(f)
+    return fs
+f1, f2, f3 = count()
+print(f1()) # 输出9
+print(f2()) # 输出9
+print(f3()) # 输出9
+# 上面全部输出9的原因在于，返回的函数并没有立即执行，而是等到3个函数都返回时，它们所引用的变量i,都已经改变成3了，因此最终结果为9
+
+# 返回闭包时，牢记一点，返回函数不要引用任何循环变量，或者后续会发生改变的变量。
+
+def count_1():
+    def f():
+        def g():
+            return i * i
+        return g
+    fs = []
+    for i in range(1, 4):
+        fs.append(f(i))
+    return fs
+f4, f5, f6 = count_1()
+print(f4())
+print(f5())
+print(f6())
