ceb332eb26a63c7c3716ae8dbb4c14e435e5f494
kapokjyz <kapok_jyz@163.com>
2018-10-09 08:28:29 +0800

    修改语法错误

--- 25_mould.py
+++ 25_mould.py
@@ -20,7 +20,7 @@ def _private_1(name):
     return 'hello, %s' % name
 def _private_2(name):
     return 'hi, %s' % name
-def greeting(name)
+def greeting(name):
     if len(name) > 3:
         return _private_1(name)
     else:
